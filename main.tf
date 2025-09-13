4c303792947b8807f02fa5e30aaedcc08ab51aa1
Vivek <dabhadev92@gmail.com>
2025-09-13 14:37:44 +0530

    second-commit

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "azurerm_virtual_network" "Demo" {
   count = 2
   name = var.vnet_name[count.index]
   resource_group_name = azurerm_resource_group.third-rg.name
-  address_space = [var.vnet_address_space[count.index]]
+  address_space = [element(var.vnet_address_space[*], count.index)]
   location = azurerm_resource_group.third-rg.location
   tags = {
     environment = "Demo"
